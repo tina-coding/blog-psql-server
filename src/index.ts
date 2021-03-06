2dc36488f93fcfca2c64be12fceb91e324437dc6
Tina G <tina@smartlogic.io>
2021-03-06 11:02:27 -0500

    ⚙ Config: Adds MikroORM and sets up entity
    ::📦 Package.json: Adds Dependencies::
    
    +@mikro-orm/cli: Creating migrations in terminal
    +@mikro-orm/core: The core functions for interacting with db
    +@mikro-orm/migrations: Creating migrations
    +@mikro-orm/postgresql: Using postgres
    +pg: Postgres client
    +nodemon: <devDep> Watches files and recompiles on file changes
    
    :: Additions ::
    
    +Constants: A file containing reusable consts
    -Adds const for checking if env is prod
    
    +Entities: A directory of model entities
    -Post: Creates an entity for a post
    
    +Migrations: A directory of db migrations
    
    +mikro-orm.config: A config file for mikro-orm that sets the options
    
    :: Modifications ::
    
    +Index: Removes console.log("hello") and adds a main executable function
    that
    - initializes a MikroORM
    - creates an instance of the post entity
    - saves the post
    - fetches the list of posts, logs list
    
    :: Migrations ::
    
    +Create Post table with
    - title
    - createdAt and updatedAt
    - id or primary key

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello");
+import { Post } from './entities/Post';
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "src/constants";
+import microConfig from './mikro-orm.config';
+
+
+const main = async () => {
+
+	const orm = await MikroORM.init(microConfig);
+	orm.getMigrator().up(); // run migrations
+
+	// creates an instance of post
+	const post = orm.em.create(Post, {
+		title: 'my first post '
+	});
+	await orm.em.persistAndFlush(post);
+
+
+	const posts = await orm.em.find(Post, {});
+	console.log({ posts });
+}
+
+main();
